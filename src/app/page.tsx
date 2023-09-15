8a090df3d3b0545d5e8165584c22478d8c5815ce
Wing-Dimas <wingdimas@gmail.com>
2023-09-15 19:07:57 +0700

    create navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Contact from "@/components/Contact";
+import Experince from "@/components/Experince";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Project from "@/components/Project";
 import React from "react";
 
 export default function page() {
-  return <div>Hello World</div>;
+  return (
+    <div className="min-h-[2000px] relative">
+      <Navbar />
+      <Hero />
+      <Experince />
+      <Project />
+      <Contact />
+    </div>
+  );
 }
